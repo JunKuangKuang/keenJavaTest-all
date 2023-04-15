db3bcc0367101dd633c01d2c2622ea0bd301000b
君匡 <1533859388@qq.com>
2023-04-15 14:51:36 +0800

    启动SpringBoot项目并退出

--- springBoot/base/src/main/java/xyz/clzly/keen/MainApplication.java
+++ springBoot/base/src/main/java/xyz/clzly/keen/MainApplication.java
@@ -1,8 +1,12 @@
 package xyz.clzly.keen;
 
+import com.apple.eawt.Application;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
@@ -17,15 +21,23 @@ import org.springframework.context.annotation.PropertySource;
 // 指定的配置文件不存在是否报错，默认是false。当设置为 true 时，若该文件不存在，程序不会报错
 @PropertySource(value = {
         "classpath:start.properties"
-    }, ignoreResourceNotFound = false)
+}, ignoreResourceNotFound = false)
 @SpringBootApplication
- public class MainApplication {
-     public static void main(String[] args) {
-         // 解除JUL的绑定
-         SLF4JBridgeHandler.removeHandlersForRootLogger();
-         SLF4JBridgeHandler.install();
+public class MainApplication {
+    public static void main(String[] args) {
+        // 解除JUL的绑定
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
 
-         SpringApplication.run(MainApplication.class);
-         System.out.println("君匡匡真了不起！");
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(MainApplication.class)
+                .web(WebApplicationType.NONE)
+                .run();
+
+        System.out.println("Spring Boot application started");
+
+        System.out.println("君匡匡真了不起！");
+
+        // 退出并结束
+        SpringApplication.exit(ctx, () -> 0);
     }
- }
+}
